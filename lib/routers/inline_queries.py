d66ab6d97a7e05f6043e859824d4993e721aae75
Matvei <matveiyan@yandex.ru>
2026-04-11 00:22:20 +0300

    remove download keyword in inline query

--- lib/routers/inline_queries.py
+++ lib/routers/inline_queries.py
@@ -2,7 +2,7 @@ from lib.downloader import downloader
 from lib.utils.general_utils import run_in_thread
 from lib.config_reader import config
 from aiogram import Router, F
-from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
+from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent, LinkPreviewOptions
 
 router = Router()
 router.inline_query.filter(
@@ -10,11 +10,11 @@ router.inline_query.filter(
 )
 
 
-async def show_invalid(inline_query: InlineQuery, text: str) -> None:
+async def show_text(inline_query: InlineQuery, title: str, text: str) -> None:
     await inline_query.answer(results=[
         InlineQueryResultArticle(
             id="default",
-            title="Invalid",
+            title=title,
             input_message_content=InputTextMessageContent(message_text=text),
         )
     ])
@@ -23,22 +23,17 @@ async def show_invalid(inline_query: InlineQuery, text: str) -> None:
 @router.inline_query()
 async def inline_handler(inline_query: InlineQuery):
     query = inline_query.query.strip()
-
     if not query:
-        return await show_invalid(inline_query, "No query provided")
+        return await show_text(inline_query, "Invalid", "No query provided")
 
-    args = query.split()
-    if len(args) != 2 or args[0] != "download":
-        return await show_invalid(inline_query, "Not enough arguments or invalid command")
-
-    url = args[1]
+    url = query.split()[0]
     result, error = await run_in_thread(downloader.download, url)
     if error:
-        return await show_invalid(inline_query, "Error occurred while downloading")
+        return await show_text(inline_query, "Error", "Error occurred while downloading")
 
     filepath, filename, server_url, info = result
     if not server_url:
-        return await show_invalid(inline_query, "Error occurred while downloading")
+        return await show_text(inline_query, "Error", "Error occurred while downloading")
 
     results = [
         InlineQueryResultArticle(
@@ -47,7 +42,13 @@ async def inline_handler(inline_query: InlineQuery):
             description="Click to open video",
             # thumbnail_url="https://picsum.photos/200",
             input_message_content=InputTextMessageContent(
-                message_text=f"Open Video: {server_url}"
+                message_text=server_url,
+                link_preview_options=LinkPreviewOptions(
+                    url=server_url,
+                    is_disabled=False,
+                    prefer_large_media=True,
+                    show_above_text=True
+                ),
             ),
         )
     ]
