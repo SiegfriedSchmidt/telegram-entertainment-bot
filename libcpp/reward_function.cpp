b276e7dba1716071fb682a86fb8ae52c8dbd0973
Nick Semenov <100042198+Nik7sem@users.noreply.github.com>
2026-05-15 00:34:23 +0300

    Easter/anniversary blocks rewards
    Anniversary and easter blocks rewards. Reward multiplier.

--- libcpp/reward_function.cpp
+++ libcpp/reward_function.cpp
@@ -11,10 +11,33 @@ extern "C" {
         constexpr double k = 1.538;
         constexpr double e = 2.71828;
 
-        if (num <= 1328) {
+        if(num <= 1328) {
             return base_reward;
         }
+
+        if(num == 1337){
+            return 1337;
+        }
+
+        if(num == 1538){
+            return 15380;
+        }
+
+        double m = 1;
+
+        if(num % 10000 == 0){
+            m = 6;
+        } else if(num % 1000 == 0){
+            m = 3;
+        } else if(num % 100 == 0){
+            m = 1.5;
+        }
+
+        if(num % 93 == 0 && num % 16 == 0 && num / 93 == 16){
+            m = 0;
+        }
+
         const double x = pow(num, 3) / pow(nonce, 2);
-        return ceil(b * sin(i * x) + p * cos(k * x) + pow(e, i));
+        return m * ceil(b * sin(i * x) + p * cos(k * x) + pow(e, i));
     }
 }
