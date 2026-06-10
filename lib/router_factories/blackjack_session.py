3221090bd5ed3098e6d4db2642900228f279d0fb
Matvei <matveiyan@yandex.ru>
2026-06-10 17:27:03 +0300

    blackjack hot fix

--- lib/router_factories/blackjack_session.py
+++ lib/router_factories/blackjack_session.py
@@ -21,13 +21,16 @@ def create_router():
         filename, lose = blackjack.hit()
 
         image = FSInputFile(filename, filename=str(filename))
-        media = InputMediaPhoto(media=image, caption="Hit!")
         if lose:
-            media.caption = blackjack.get_caption_and_record_gain(BlackjackResultType.bust)
+            media = InputMediaPhoto(
+                media=image,
+                caption=blackjack.get_caption_and_record_gain(BlackjackResultType.bust)
+            )
             await state.clear()
             return await callback.message.edit_media(media)
-
-        return await callback.message.edit_media(media, reply_markup=get_blackjack_keyboard(blackjack.user.id))
+        else:
+            media = InputMediaPhoto(media=image, caption="Hit!")
+            return await callback.message.edit_media(media, reply_markup=get_blackjack_keyboard(blackjack.user.id))
 
     @router.callback_query(BlackjackCallback.filter(F.action == "stand"))
     async def stand_cmd(callback: types.CallbackQuery, state: FSMContext):
